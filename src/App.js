18560a77dcdf55202fa2302b310c386089bb22c2
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2022-01-27 11:33:39 +0000

    Fixed wrap on skill and added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Nav from "./components/Nav";
+import Footer from "./components/Footer"
 import Home from "./components/Home"
 import Dosha from "./components/Dosha";
 import Notes from "./components/Notes";
@@ -17,6 +18,7 @@ function App() {
           <Route path="notes" element={<Notes />} />
         </Routes>
       </div>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,39 @@
+import { FaTwitter } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+
 const Footer = () => {
-    return ( <footer>Footer</footer> );
+    return (
+      <footer>
+        <p>Debbie Dann © 2022</p>
+        <div className="footer-social-links">
+          <a
+            href="https://twitter.com/debbie_digital"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Twitter"
+          >
+            <FaTwitter className="social-icon" />
+          </a>
+          <a
+            href="https://github.com/hellodeborahuk"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/debbiedann/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn"
+          >
+            <FaLinkedinIn />
+          </a>
+        </div>
+      </footer>
+    );
 }
  
 export default Footer;
