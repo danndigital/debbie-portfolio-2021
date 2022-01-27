9aeb3979f49ea70e890ac1e6cee4eae2f536ac09
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2022-01-27 15:45:52 +0000

    I don't know what's going on with this page to be honest

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,62 +1,91 @@
 const Projects = () => {
-  const project = [
-    {
-      name: "Notes App",
-      thumbnail: "../images/notes.png",
-      copy: "A CRUD application to store and edit notes using React and TailwindCSS.",
-      caseStudy: "/Notes",
-      link: "https://awesome-lamport-4fcaff.netlify.app/",
-      github: "https://github.com/hellodeborahuk/coding-notebook",
-    },
-    {
-      name: "React Weather App",
-      thumbnail: "../images/yoga-website.png",
-      copy: "A weather app built with React and API intergration for my SheCodes Workshop project.",
-      caseStudy: "/WeatherApp",
-      link: "https://ecstatic-swartz-7b404c.netlify.app/",
-      github: "https://github.com/hellodeborahuk/react-weather-app",
-    },
-    {
-      name: "Ayurveda Dosha Quiz",
-      thumbnail: "../images/dosha.png",
-      copy: "I created a quiz from scratch to implement what I've learnt about React.",
-      caseStudy: "/Dosha",
-      link: "https://cranky-curran-3d406a.netlify.app/",
-      github: "https://github.com/hellodeborahuk/dosha-quiz",
-    },
-  ];
 
   return (
     <section>
       <h2 className="projects-title">Projects</h2>
       <div className="projects">
-        {project.map(function (project, index) {
-            return (
-              <article key={index} className="project-card">
-                <div className="project-img-container">
-                  <img
-                    src={project.thumbnail}
-                    alt={project.name}
-                    className="project-img"
-                  />
-                  <div className="project-img-background"></div>
-                </div>
-                <div className="project-details">
-                  <h3>{project.name}</h3>
-                  <p>{project.copy}</p>
-                  <div>
-                    <p>React</p>
-                    <p>TailwindCSS</p>
-                  </div>
-                  <div>
-                    <a href="/">Case study</a>
-                    <a href="/">Live</a>
-                    <a href="/">GitHub</a>
-                  </div>
-                </div>
-              </article>
-            );
-        })}
+        <article className="project-card">
+          <div className="project-img-container">
+            <img
+              src="../images/notes.png"
+              alt="Notes App"
+              className="project-img"
+            />
+            <div className="project-img-background"></div>
+          </div>
+          <div className="project-details">
+            <h3 className="project-title">Notes app</h3>
+            <p>
+              A CRUD application to store and edit notes using React and
+              TailwindCSS.
+            </p>
+            <div className="project-tech">
+              <p>React</p>
+              <p>TailwindCSS</p>
+            </div>
+            <div className="project-links">
+              <a href="/">Case study</a>
+              <a href="https://awesome-lamport-4fcaff.netlify.app/">Live</a>
+              <a href="https://github.com/hellodeborahuk/coding-notebook">
+                GitHub
+              </a>
+            </div>
+          </div>
+        </article>
+
+        <article className="project-card">
+          <div className="project-img-container">
+            <img
+              src="../images/notes.png"
+              alt="Notes App"
+              className="project-img"
+            />
+            <div className="project-img-background"></div>
+          </div>
+          <div className="project-details">
+            <h3 className="project-title">Notes app</h3>
+            <p className="project-copy">
+              A CRUD application to store and edit notes using React and
+              TailwindCSS.
+            </p>
+            <div className="project-tech">
+              <p>React</p>
+              <p>TailwindCSS</p>
+            </div>
+            <div className="project-links">
+              <a href="/">Case study</a>
+              <a href="/">Live</a>
+              <a href="/">GitHub</a>
+            </div>
+          </div>
+        </article>
+
+        <article className="project-card">
+          <div className="project-img-container">
+            <img
+              src="../images/dosha.png"
+              alt="Dosha quiz"
+              className="project-img"
+            />
+            <div className="project-img-background"></div>
+          </div>
+          <div className="project-details">
+            <h3 className="project-title">Ayurveda Dosha Quiz</h3>
+            <p>
+              I created a quiz from scratch to implement what I've learnt about
+              React.
+            </p>
+            <div className="project-tech">
+              <p>React</p>
+              <p>TailwindCSS</p>
+            </div>
+            <div className="project-links">
+              <a href="/">Case study</a>
+              <a href="https://cranky-curran-3d406a.netlify.app/">Live</a>
+              <a href="https://github.com/hellodeborahuk/dosha-quiz">GitHub</a>
+            </div>
+          </div>
+        </article>
       </div>
     </section>
   );
