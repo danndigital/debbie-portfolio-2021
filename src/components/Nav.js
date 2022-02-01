507555c375e49690ec9c654ff4e661e0bb74a899
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2022-02-01 21:34:45 +0000

    Added responsive navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,48 +1,63 @@
 import { FaTwitter } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi"
+import {MdClose} from "react-icons/md"
 import { Link } from "react-router-dom";
+import {useState} from "react";
+
 
 const Nav = () => {
+  const [active, setActive] = useState(false)
+
+  const showMenu = () => {
+    setActive(!active)
+  }
+
+
   return (
     <nav>
       <Link to="/" className="nav-title">
         <h1>debbie dann digital</h1>
       </Link>
-      <div className="links">
-        <div className="nav-links">
-          <Link to="/">Home</Link>
-          <a href="#projects">Projects</a>
-          <a href="#about-section">About</a>
-          <a href="#contact">Contact</a>
-        </div>
-        <div className="social-links">
-          <a
-            href="https://twitter.com/debbie_digital"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Twitter"
-          >
-            <FaTwitter className="social-icon" />
-          </a>
-          <a
-            href="https://github.com/hellodeborahuk"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="GitHub"
-          >
-            <FaGithub />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/debbiedann/"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="LinkedIn"
-          >
-            <FaLinkedinIn />
-          </a>
-        </div>
+      <div className="menu-icon">
+        <GiHamburgerMenu className="menu" onClick={showMenu} />
       </div>
+        <div className="social-links">
+            <a
+              href="https://twitter.com/debbie_digital"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Twitter"
+            >
+              <FaTwitter className="social-icon" />
+            </a>
+            <a
+              href="https://github.com/hellodeborahuk"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="GitHub"
+            >
+              <FaGithub />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/debbiedann/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="LinkedIn"
+            >
+              <FaLinkedinIn />
+            </a>
+          </div>
+        <div className={active ? 'slider active' : 'slider'}>
+            <div className="nav-links">
+              <div className="closed"><MdClose className="close" onClick={showMenu} /></div>
+              <Link to="/">Home</Link>
+              <a href="#projects">Projects</a>
+              <a href="#about-section">About</a>
+              <a href="#contact">Contact</a>
+            </div>
+         </div>
     </nav>
   );
 };
