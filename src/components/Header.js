1f803baa995013b4d3ffd67bcfc0079eb259a0ec
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2022-02-01 09:00:22 +0000

    gsap animated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,87 @@
+import { useRef, useEffect } from "react";
+import { gsap, Power3 } from "gsap";
+
 const Header = () => {
-    return (
-      <header>
-        <section className="header-section">
-          <h1 className="header-title">
-            I am a frontend developer.
-          </h1>
-          <p className="header-emphasis">
-            Hello, I'm Debbie. I design and code beautifully simple things.
-          </p>
-          <p className="header-copy">
-            My social media knowledge and experience of content creation help me
-            to deliver beautiful websites with JavaScript, React and
-            TailwindCSS.
-          </p>
-          <button className="header-btn">Get in touch</button>
-        </section>
-        <img
-          src="./images/debbie-dann.png"
-          className="header-img"
-          alt="debbie dann profile shot"
-        />
-      </header>
-    );
-}
- 
+  let headerSection = useRef(null)
+  let headerTitle = useRef(null);
+  let headerEmphasis = useRef(null);
+  let headerCopy = useRef(null);
+  let headerBtn = useRef(null);
+
+  useEffect(() => {
+gsap.to(headerSection, 0, {css: {visibility: "visible"}})
+gsap.from(headerTitle, {duration: .8, opacity: 0, y: 40, ease: Power3.easeOut})
+gsap.from(headerEmphasis, {
+  duration: 0.8,
+  opacity: 0,
+  y: 40,
+  ease: Power3.easeOut,
+  delay: .2
+});
+gsap.from(headerCopy, {
+  duration: 0.8,
+  opacity: 0,
+  y: 40,
+  ease: Power3.easeOut,
+  delay: .4
+});
+
+gsap.from(headerBtn, {
+  duration: 0.8,
+  opacity: 0,
+  y: 40,
+  ease: Power3.easeOut,
+  delay: 0.6,
+});
+
+  }, []);
+
+  return (
+    <header>
+      <section ref={(el) => {
+        headerSection = el
+      }} className="header-section">
+        <h1
+          ref={(el) => {
+            headerTitle = el;
+          }}
+          className="header-title"
+        >
+          I am a frontend developer.
+        </h1>
+        <p
+          ref={(el) => {
+            headerEmphasis = el;
+          }}
+          className="header-emphasis"
+        >
+          Hello, I'm Debbie. I design and code beautifully simple things.
+        </p>
+        <p
+          ref={(el) => {
+            headerCopy = el;
+          }}
+          className="header-copy"
+        >
+          My social media knowledge and experience of content creation help me
+          to deliver beautiful websites with JavaScript, React and TailwindCSS.
+        </p>
+        <button
+          ref={(el) => {
+            headerBtn = el;
+          }}
+          className="header-btn"
+        >
+          Get in touch
+        </button>
+      </section>
+      <img
+        src="./images/debbie-dann.png"
+        className="header-img"
+        alt="debbie dann profile shot"
+      />
+    </header>
+  );
+};
+
 export default Header;
